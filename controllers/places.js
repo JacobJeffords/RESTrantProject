8c7cd6fe506660ab018d56e6552c8adf670b2642
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-05-06 18:39:33 -0400

    Show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -7,22 +7,38 @@ router.get('/', (req, res) => {
 
 router.get('/new', (req, res) => {
     res.render('places/new')
-  })
-  
-  router.post('/', (req, res) => {
+})
+
+router.get('/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+        res.render('places/show', { place: places[id] })
+    }
+})
+
+
+
+
+router.post('/', (req, res) => {
     console.log(req.body)
     if (!req.body.pic) {
-      req.body.pic = 'http://placekitten.com/400/400'
+        req.body.pic = 'http://placekitten.com/400/400'
     }
     if (!req.body.city) {
-      req.body.city = 'Anytown'
+        req.body.city = 'Anytown'
     }
     if (!req.body.state) {
-      req.body.state = 'USA'
+        req.body.state = 'USA'
     }
-    places.push (req.body)
+    places.push(req.body)
     res.redirect('/places')
-  })
-  
+})
+
 
 module.exports = router
