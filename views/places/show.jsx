df9dc5cb6b374e051222dc49cb1e0f9f6e3e9380
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-09-18 13:24:22 -0400

    Mongoose properly installed

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -7,56 +7,79 @@ function show(data) {
             No comments yet!
         </h3>
     )
-    if (data.place.comments.length) {
-        comments = data.place.comments.map(c => {
-            return (
-                <div className="border">
-                    <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
-                    <h4>{c.content}</h4>
-                    <h3>
-                        <stong>- {c.author}</stong>
-                    </h3>
-                    <h4>Rating: {c.stars}</h4>
-                </div>
-            )
-        })
-    }
-    return (
-        <Def>
-            <main>
-                <div>
-                    <h1>{data.place.name}</h1>
-                </div>
-                <div>
-                    <img src={data.place.pic} />
-                </div>
-                <div>
-                    <p>Located in {data.place.city}, {data.place.state}<br></br>
-                        Serving {data.place.cuisines}</p>
-                </div>
 
-                <div>
-                    <h2>Ratings</h2>
-                    <p>Not yet rated</p>
-                </div>
-
-                <div>
-                    <h2>Comments</h2>
-                    {comments}
-                </div>
-
-                <a href={`/places/${data.id}/edit`} className="btn btn-warning">
-                    Edit
-                </a>
-                <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
-                    <button type="submit" className="btn btn-danger">
-                        Delete
-                    </button>
-                </form>
-
-            </main>
-        </Def>
+    let rating = (
+        <h3 className="inactive">
+            Not yet rated
+        </h3>
     )
-}
+    if (data.place.comments.length) {
+        let sumRatings = data.place.comments.reduce((tot, c) => {
+            return tot + c.stars
+        }, 0)
+        let averageRating = sumRatings / data.place.comments.length
+        rating = (
+            <h3>
+                {averageRating} stars
+            </h3>
+        )
 
-module.exports = show
+        if (data.place.comments.length) {
+            comments = data.place.comments.map(c => {
+                return (
+                    <div className="border">
+                        <h2 className="rant">{c.rant ? 'Rant! ðŸ˜¡' : 'Rave! ðŸ˜»'}</h2>
+                        <h4>{c.content}</h4>
+                        <h3>
+                            <stong>- {c.author}</stong>
+                        </h3>
+                        <h4>Rating: {c.stars}</h4>
+                    </div>
+                )
+            })
+        }
+        return (
+            <Def>
+                <main>
+                    <div>
+                        <h1>{data.place.name}</h1>
+                        <h2>
+                            <p>Rating</p>
+                        </h2>
+                        {rating}
+                        <br />
+                    </div>
+                    <div>
+                        <img src={data.place.pic} />
+                    </div>
+                    <div>
+                        <p>Located in {data.place.city}, {data.place.state}<br></br>
+                            Serving {data.place.cuisines}</p>
+                    </div>
+
+                    <div>
+                        <h2>Ratings</h2>
+                        <p>Not yet rated</p>
+                    </div>
+
+                    <div>
+                        <h2>Comments</h2>
+                        {comments}
+                    </div>
+
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+                        Edit
+                    </a>
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>
+
+                </main>
+            </Def>
+        )
+    }
+
+    module.exports = show
+}
